25bb0806ee9960cb4b4b83f10e550c9823325cd6
zhuba-Ahhh <3477826311@qq.com>
2024-05-01 20:02:44 +0800

    feat: :sparkles: update lexical

--- src/Views/Tiptap/index.tsx
+++ src/Views/Tiptap/index.tsx
@@ -1,11 +1,10 @@
-import "./index.css";
-
 import { Color } from "@tiptap/extension-color";
 import ListItem from "@tiptap/extension-list-item";
 import TextStyle from "@tiptap/extension-text-style";
 import { EditorProvider, useCurrentEditor } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 import React from "react";
+import "./index.css";
 
 const MenuBar = () => {
   const { editor } = useCurrentEditor();
@@ -198,7 +197,9 @@ const Tiptap = () => {
       slotBefore={<MenuBar />}
       extensions={extensions}
       content={content}
-    />
+    >
+      <div></div>
+    </EditorProvider>
   );
 };
 
